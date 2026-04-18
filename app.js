e0bdf75082e3da51c7e0f8651e2b8c66341b20cd
RajBhokare <bhokareraj281@gmail.com>
2026-04-18 16:29:32 +0530

    login page

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
-import express from "express" ;
-import dotenv from "dotenv" ;
-import axios from "axios" ;
+import express from "express";
+import dotenv from "dotenv";
+import axios from "axios";
 
-dotenv.config() ;
+dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json()); 
 app.set("view engine", "ejs");
 
-app.get("/" , async (req , res) => {
-  res.render("home.ejs") ;
+app.get("/", async (req, res) => {
+  res.render("home.ejs");
 });
 
-app.listen(port , ()=> {
-  console.log(`Listening on port http://localhost:${port}`) ;
+app.get("/features", async (req, res) => {
+  res.render("features.ejs");
+});
+
+app.get("/working", async (req, res) => {
+  res.render("working.ejs");
+});
+
+app.get("/login", async (req, res) => {
+  res.render("login.ejs");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port http://localhost:${port}`);
 });
